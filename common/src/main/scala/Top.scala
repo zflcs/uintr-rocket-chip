0181e04c3d673404cdacd877c903e6e003b70af8
TKF <kaifu6821@qq.com>
2023-04-14 20:17:43 +0800

    Add RISC-V N-extension tests

--- common/src/main/scala/Top.scala
+++ common/src/main/scala/Top.scala
@@ -5,6 +5,6 @@ import chisel3._
 /**
   * Top module for zynq fpga, which will be wrapped with SoC block design.
   */
-class ZynqTop(implicit p: Parameters) extends Module {
+class ZynqTop() extends Module {
   // TODO
 }

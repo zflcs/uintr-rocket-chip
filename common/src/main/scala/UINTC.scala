caa0ba0965051d611b96b6a6e476f47fdd0e7565
田凯夫 <tkf19@mails.tsinghua.edu.cn>
2023-04-25 12:54:51 +0800

    Support UINTC(64 bit)

--- common/src/main/scala/UINTC.scala
+++ common/src/main/scala/UINTC.scala
@@ -1,11 +1,160 @@
 package freechips.rocketchip.uintr
 
-import chisel3._
-import freechips.rocketchip.config.Parameters
+import Chisel._
+import freechips.rocketchip.config.{Field, Parameters}
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.interrupts._
+import freechips.rocketchip.regmapper._
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.util._
 
-/**
-  * User-Interrupt Controller
-  */
-class UINTC(implicit p: Parameters) extends Module {
-  // TODO
+class UIRS extends Bundle {
+  val pending = UInt(64.W)
+  val zero1 = UInt(32.W)
+  val hartid = UInt(16.W)
+  val zero0 = UInt(14.W)
+  val mode = Bool()
+  val active = Bool()
+}
+
+object UINTCConsts {
+  def size = 0x4000
+
+  def opBytes = 32
+
+  def nRecvs = size / opBytes
+
+  def opOffset(index: Int) = index * opBytes
+
+  def sendOffset(index: Int) = opOffset(index) + 0x00
+
+  def lowOffset(index: Int) = opOffset(index) + 0x08
+
+  def highOffset(index: Int) = opOffset(index) + 0x10
+
+  def actOffset(index: Int) = opOffset(index) + 0x18
+}
+
+case class UINTCParams(baseAddress: BigInt = 0x03000000, intStages: Int = 0) {
+  def address = AddressSet(baseAddress, UINTCConsts.size - 1)
+}
+
+case object UINTCKey extends Field[Option[UINTCParams]](None)
+
+case class UINTCAttachParams(slaveWhere: TLBusWrapperLocation = CBUS)
+
+case object UINTCAttachKey extends Field(UINTCAttachParams())
+
+/** User-Interrupt Controller */
+class UINTC(params: UINTCParams, beatBytes: Int)(implicit p: Parameters) extends LazyModule {
+
+  import UINTCConsts._
+
+  val device = new SimpleDevice("uintc", Seq("riscv,uintc0")) {
+    override val alwaysExtended: Boolean = true
+
+    override def describe(resources: ResourceBindings): Description = {
+      val Description(name, mapping) = super.describe(resources)
+      val extra = Map("interrupt-controller" -> Nil, "#interrupt-cells" -> Seq(ResourceInt(1)))
+      Description(name, mapping ++ extra)
+    }
+  }
+
+  val node: TLRegisterNode = TLRegisterNode(
+    address = Seq(params.address),
+    device = device,
+    beatBytes = beatBytes,
+    concurrency = 1) // limiting concurrency handles RAW hazards on claim registers
+
+  /* Only ouput */
+  val intnode: IntNexusNode = IntNexusNode(
+    sourceFn = { _ => IntSourcePortParameters(Seq(IntSourceParameters(1, Seq(Resource(device, "int"))))) },
+    sinkFn = { _ => IntSinkPortParameters(Seq(IntSinkParameters())) },
+    outputRequiresInput = false)
+
+  /* Negotiated sizes */
+  def nHarts = intnode.edges.out.map(_.source.num).sum
+
+  def hartId(id: UInt) = if (nHarts <= 1) 0.U else id(log2Ceil(nHarts) - 1, 0)
+
+  lazy val module = new LazyModuleImp(this) {
+    Annotated.params(this, params)
+    require(intnode.edges.in.size == 0, "UINTC only produces interrupts; it does not accept them")
+
+    val uirs = Seq.fill(nRecvs) { RegInit(new UIRS().fromBits(0.U)) }
+
+    val ipi = Seq.fill(nHarts) { RegInit(0.U) }
+    ipi.zipWithIndex.foreach { case (hart, i) =>
+      hart := uirs.map(x => x.pending =/= 0.U && x.active && hartId(x.hartid) === i.asUInt).reduce(_ || _)
+    }
+    val (intnode_out, _) = intnode.out.unzip
+    intnode_out.zipWithIndex.foreach { case (int, i) =>
+      int(0) := ShiftRegister(ipi(i)(0), params.intStages) // usip
+    }
+
+    /* UINTC operation fields */
+    val opRegFields = uirs.zipWithIndex.flatMap { case (x, i) =>
+      Seq(
+        sendOffset(i) -> Seq(RegField(64, (),
+          RegWriteFn { (valid, data) =>
+            x.pending := x.pending | (valid << data(5, 0)).asUInt
+            Bool(true)
+          },
+          Some(RegFieldDesc(
+            name = s"send_uipi_$i",
+            desc = s"Send user software interrupt to receiver $i if bits set to 1.")))),
+        lowOffset(i) -> Seq(RegField(64,
+          RegReadFn { ready => (Bool(true), x.asUInt(63, 0)) },
+          RegWriteFn { (valid, data) =>
+            when(valid) {
+              x.hartid := data(31, 16)
+              x.mode := data(1)
+              x.active := data(0)
+            }
+            Bool(true)
+          },
+          Some(RegFieldDesc(
+            name = s"uirs_low_bits_$i",
+            desc = s"User interrupt basic status (active, mode and target hartid) of receiver $i")))),
+        highOffset(i) -> Seq(RegField(64,
+          RegReadFn { ready =>
+            when(ready) { x.pending := 0.U }
+            (Bool(true), x.pending)
+          },
+          RegWriteFn { (valid, data) =>
+            when(valid) { x.pending := x.pending | data }
+            Bool(true)
+          },
+          Some(RegFieldDesc(
+            name = s"uirs_high_bits_$i",
+            desc = s"One bit for each user interrupt vector. There is user-interrupt request for a vector if the corresponding bit is 1.",
+            wrType = Some(RegFieldWrType.ONE_TO_SET),
+            rdAction = Some(RegFieldRdAction.CLEAR))))),
+        actOffset(i) -> Seq(RegField(64, (),
+          RegWriteFn { (valid, data) =>
+            when(valid) { x.active := data(0) }
+            Bool(true)
+          }, None))
+      )
+    }
+    node.regmap((opRegFields): _*)
+  }
+}
+
+/** Trait that will connect a UINTC to a subsystem */
+trait CanHavePeripheryUINTC {
+  this: BaseSubsystem =>
+  val uintcOpt = p(UINTCKey).map { params =>
+    val tlbus = locateTLBusWrapper(p(UINTCAttachKey).slaveWhere)
+    val uintc = LazyModule(new UINTC(params, cbus.beatBytes))
+    uintc.node := tlbus.coupleTo("uintc") { TLFragmenter(tlbus) := _ }
+
+    InModuleBody {
+      uintc.module.clock := tlbus.module.clock
+      uintc.module.reset := tlbus.module.reset
+    }
+
+    uintc
+  }
 }

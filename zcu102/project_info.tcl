feee7b2c3102b4e737f0050700901c1ac2700d8c
Gallium70 <52118815+Gallium70@users.noreply.github.com>
2023-05-27 17:13:18 +0800

    add board_part to project info

--- zcu102/project_info.tcl
+++ zcu102/project_info.tcl
@@ -2,7 +2,7 @@
 proc set_project_properties_post_create_project {proj_name} {
     set project_obj [get_projects $proj_name]
     set_property "part" "xczu9eg-ffvb1156-2-e" $project_obj
-    set_property "board_part" "" $project_obj
+    set_property "board_part" "xilinx.com:zcu102:part0:3.4" $project_obj
     set_property "default_lib" "xil_defaultlib" $project_obj
     set_property "simulator_language" "Mixed" $project_obj
     set_property "target_language" "Verilog" $project_obj

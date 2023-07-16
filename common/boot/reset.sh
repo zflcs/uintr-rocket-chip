86c413d3c0cc95bdc72450fc89072ee4d2eedb3f
zflcs <1491657576@qq.com>
2023-07-16 10:54:19 +0800

    modify axu15eg reset.sh

--- common/boot/reset.sh
+++ common/boot/reset.sh
@@ -5,3 +5,12 @@ echo out > /sys/class/gpio511/direction
 echo 1 > /sys/class/gpio511/value
 sleep 0.1
 echo 0 > /sys/class/gpio511/value
+
+
+# axu15eg
+# set -x
+# echo 333 > /sys/class/gpio/export 2> /dev/null
+# echo out > /sys/class/gpio/gpio333/direction
+# echo 1 > /sys/class/gpio/gpio333/value
+# sleep 0.1
+# echo 0 > /sys/class/gpio/gpio333/value
